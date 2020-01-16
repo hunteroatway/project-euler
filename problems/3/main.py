5d098f01a560dc3e97532985dbc77dd8303e8491
hunteroatway <oatway.h@gmail.com>
2020-01-16 12:02:05 -0600

    fixing tabs?

--- problems/3/main.py
+++ problems/3/main.py
@@ -1,13 +1,13 @@
 n = 600851475143
 
 def lpf(n):
-	p = 2
-	while n > p:
-		if n % p  == 0:
-			n = n / p
-			p = 2
-		else:
-			p += 1
-	return p
+  p = 2
+  while n > p:
+    if n % p  == 0:
+      n = n / p
+      p = 2
+    else:
+      p += 1
+  return p
 
 print(lpf(n))
